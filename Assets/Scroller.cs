560218ec66f6c85d29c22a81b5cac727243337c2
amd-nsr <ahmed.abdelnasser.abbas@gmail.com>
2022-07-02 00:19:00 +0200

    Object scrolling

---
+++ Assets/Scroller.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+ 
+public class Scroller : MonoBehaviour {
+   private float xoff ,yoff;
+	void Update() {
+		xoff = Time.time * 1f;
+		yoff = Time.time * 0f;
+		GetComponent<Renderer> ().material.mainTextureOffset = new Vector2 (xoff, yoff);
+	}   } 
+
